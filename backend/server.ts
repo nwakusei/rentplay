b66bd179e09d3da4dba0b1806dd7545938487ff5
Reinaldo Guedes <rguedes.dev@gmail.com>
2025-04-26 02:42:44 -0300

    Comentario feito para testes.

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import http from "http";
 const hostname = "127.0.0.1";
 const port = 5000;
 
+// Server configuration
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader("Content-type", "text/plain");
